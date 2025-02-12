39b8a6564b5b5fb2251dddd00f415451132f7510
Carlangatrex <105115623+Carlangatrex@users.noreply.github.com>
2025-02-12 12:37:55 -0500

    Update palabras.js

--- palabras.js
+++ palabras.js
@@ -1,5 +1,15 @@
-const words = ['Carne','Martillo', 'Lavadora','Sucio',
-    'Cangrejo','Lento','Alimentos','Delgado','Cubo',
-    'Comida','Caracol','Abajo','Alumno','Bonito',
-    'Cesta','Sol','Beber','Botella','Hamburguesa',
-    'Invierno'];
+const englishWords = ['Boy','Tree','Loved','Play',
+    'Best','Friends','Grew','Asked','Apples',
+    'Wood','Boat','Money','Old','Last',
+    'Time','Sit','Rest','Once Upon a Time','Upon',
+    'Time'];
+
+const frenchWords = ['Garçon','Arbre','Aimé','Jouer',
+    'Meilleur','Amis','Grandir','Demandé','Pommes',
+    'Bois','Bateau','Argent','Vieux','Dernier',
+    'Temps','Asseoir','Repos','Il était une fois'];
+
+const spanishWords = ['Niño','Árbol','Amado','Jugar',
+    'Mejor','Amigos','Creció','Preguntó','Manzanas',
+    'Madera','Barco','Dinero','Viejo','Último',
+    'Tiempo','Sentar','Descansar','Había una vez'];
