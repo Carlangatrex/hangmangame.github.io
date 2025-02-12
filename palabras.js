00e983d7bbb69f9c7b37e5e1b927b9e2e9eed3cb
Carlangatrex <carlangatrex@gmail.com>
2025-02-12 10:55:34 -0500

    Actualizar archivos con nuevas funcionalidades y estilos

--- palabras.js
+++ palabras.js
@@ -1,7 +1,7 @@
 const englishWords = ['Boy','Tree','Loved','Play',
     'Best','Friends','Grew','Asked','Apples',
     'Wood','Boat','Money','Old','Last',
-    'Time','Sit','Rest','Once','Upon',
+    'Time','Sit','Rest','Once Upon a Time','Upon',
     'Time'];
 
 const frenchWords = ['Garçon','Arbre','Aimé','Jouer',
