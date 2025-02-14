5a9a05465e5ae8703d7d19b2f5a465ba08195896
Carlangatrex <105115623+Carlangatrex@users.noreply.github.com>
2025-02-14 15:58:13 -0500

    Update palabras.js

--- palabras.js
+++ palabras.js
@@ -1,15 +1,15 @@
 const englishWords = ['Boy','Tree','Loved','Play',
     'Best','Friends','Grew','Asked','Apples',
     'Wood','Boat','Money','Old','Last',
-    'Time','Sit','Rest','Once Upon a Time','Upon',
+    'Time','Sit','Rest','Upon',
     'Time'];
 
 const frenchWords = ['Garçon','Arbre','Aimé','Jouer',
     'Meilleur','Amis','Grandir','Demandé','Pommes',
     'Bois','Bateau','Argent','Vieux','Dernier',
-    'Temps','Asseoir','Repos','Il était une fois'];
+    'Temps','Asseoir','Repos'];
 
 const spanishWords = ['Niño','Árbol','Amado','Jugar',
     'Mejor','Amigos','Creció','Preguntó','Manzanas',
     'Madera','Barco','Dinero','Viejo','Último',
-    'Tiempo','Sentar','Descansar','Había una vez'];
+    'Tiempo','Sentar','Descansar'];
